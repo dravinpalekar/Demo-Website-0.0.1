ec410e9533a4a206fe4133beeb5b8897e9fd4f40
dravinpalekar <dravin.palekar@gmail.com>
2026-07-05 19:15:10 +0530

    first commit and create multi-moudle project, adding restApi module

---
+++ demoBackend/restApi/src/test/java/dravin/com/restApi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dravin.com.restApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
