b4a0a379de264b1e302a004108e5f6f4f3baa27f
dravinpalekar <dravin.palekar@gmail.com>
2026-07-06 23:17:06 +0530

    adding 3 new features like banner, active profile and repository module
    adding 3 new features like banner name while running project, create active profile like development, staging or production level and repository module for common module.

--- demoBackend/restApi/src/main/java/dravin/com/restApi/RestApiApplication.java
+++ demoBackend/restApi/src/main/java/dravin/com/restApi/RestApiApplication.java
@@ -3,7 +3,7 @@ package dravin.com.restApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "dravin.com")
 public class RestApiApplication {
 
 	public static void main(String[] args) {
